00bb870410b80908980f1c5447a97ecd30b0539c
Romina Aguado <rominaaguado@Rominas-MacBook-Pro.local>
2021-01-06 18:12:45 -0600

    adding view functions

--- employeeCode.js
+++ employeeCode.js
@@ -69,32 +69,30 @@ function runEmployeeManagment() {
       }
     });
 }
-function viewEmployees() {
-  let query = "SELECT * FROM employee_info";
+function viewDepartments() {
+  let query =
+    "SELECT employee_info.first_name, employee_info.last_name, department_info.department_name AS Department FROM employee_info JOIN role_id ON employee_info.role_id = role_info.id JOIN department_info ON role_info.department_id = department_info.id ORDER BY employee_info.id;";
   connection.query(query, function (err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
+    runEmployeeManagment();
   });
-  // inquirer
-  //   .prompt({
-  //     name: "employee",
-  //     type: "input",
-  //     message: "VIEW Employees",
-  //   })
-  //   .then(function (answer) {
-  //     let query = "SELECT * FROM employee_info";
-  //     connection.query(query, { artist: answer.artist }, function (err, res) {
-  //       for (var i = 0; i < res.length; i++) {
-  //         console.log(
-  //           "Position: " +
-  //             res[i].position +
-  //             " || Song: " +
-  //             res[i].song +
-  //             " || Year: " +
-  //             res[i].year
-  //         );
-  //       }
-  //       runEmployeeManagment();
-  //     });
-  //   });
+}
+function viewEmployees() {
+  let query =
+    "SELECT employee_info.first_name, employee_info.last_name, role_info.title, role_info.salary, department_info.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee_info INNER JOIN role on role_info.id = employee_info.role_id INNER JOIN department_info on department_info.id = role_info.department_id left join employee e on employee_info.manager_id = e.id;";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runEmployeeManagment();
+  });
+}
+function viewRoles() {
+  let query =
+    "SELECT employee_info.first_name, employee_info.last_name, role_info.title AS Title FROM employee_info JOIN role_info ON employee_info.role_id = role_info.id;";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runEmployeeManagment();
+  });
 }
